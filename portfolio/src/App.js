ac5819fe2473174e41b7558298cef4661df768bd
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-07-20 11:00:16 +0530

    first

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Hello!!</h1>
+      <h2 className="text-3xl font-serif">Hi</h2>
+    </div>
+  );
+}
+
+export default App;
